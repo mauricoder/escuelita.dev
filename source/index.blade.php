7985ddc25177ddbd7876a71b05652ad8e6d5ce63
Mauricio Etcheverry <mauricio@yoreparo.com>
2020-12-05 22:12:55 -0300

    Curso Scratch en Home

--- source/index.blade.php
+++ source/index.blade.php
@@ -85,35 +85,10 @@
                 <h1>Cursos de Programación<br />para Niños y Jóvenes</h1>
             </div>
             <div class="row">
+                @include('_partials.card-programacion-ninos-scratch')
+
                 @include('_partials.card-app-inventor')                
-{{--            <div class="col-lg-4 col-md-6 col-sm-12 block-column">
-                    <div class="inner-block wow fadeInUp" data-wow-delay="300ms" data-wow-duration="1500ms">
-                        <figure class="image-box"><a href="class-details.html"><img src="/assets/images/resource/class-2.jpg" alt=""></a></figure>
-                        <div class="lower-content">
-                            <div class="link-btn"><a href="class-details.html"><i class="flaticon-next"></i></a></div>
-                            <h3><a href="class-details.html">Paper Plates Art</a></h3>
-                            <div class="price">$580</div>
-                            <ul class="info-box">
-                                <li>Age: <span>2-4 Years</span></li>
-                                <li>Size: <span>12 Seats</span></li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-md-6 col-sm-12 block-column">
-                    <div class="inner-block wow fadeInUp" data-wow-delay="600ms" data-wow-duration="1500ms">
-                        <figure class="image-box"><a href="class-details.html"><img src="/assets/images/resource/class-3.jpg" alt=""></a></figure>
-                        <div class="lower-content">
-                            <div class="link-btn"><a href="class-details.html"><i class="flaticon-next"></i></a></div>
-                            <h3><a href="class-details.html">Education Lessons</a></h3>
-                            <div class="price">$550</div>
-                            <ul class="info-box">
-                                <li>Age: <span>2-4 Years</span></li>
-                                <li>Size: <span>12 Seats</span></li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>--}}
+
             </div>
         </div>
     </section>
