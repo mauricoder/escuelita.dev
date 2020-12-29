fa639c9875fd85db10d08703d656ce63933c748a
Mauricio Etcheverry <mauricio@yoreparo.com>
2020-12-29 13:38:12 -0300

    Correcciones fecha lanzamiento Scratch

--- source/cursos/programacion-divertida-con-scratch-para-ninos.blade.php
+++ source/cursos/programacion-divertida-con-scratch-para-ninos.blade.php
@@ -127,11 +127,11 @@
                                 </li>
                                 <li>
                                     <h5>Cantidad de encuentros:</h5>
-                                    <span>12 clases (una por semana)</span>
+                                    <span>6 clases (una por semana)</span>
                                 </li>
                                 <li>
                                     <h5>Comienzo:</h5>
-                                    <span>PROXIMAMENTE</span>
+                                    <span>Jueves 14 de Enero (18hs de Argentina)</span>
                                 </li>                                
                             </ul>
                         </div>
