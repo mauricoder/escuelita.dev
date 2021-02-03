ac6456dd423bb2b4fd354c76921cb7a391facd5a
Mauricio Etcheverry <mauricio@yoreparo.com>
2021-02-03 00:02:44 -0300

    Update vamos-a-programar-con-python.blade.php

--- source/cursos/vamos-a-programar-con-python.blade.php
+++ source/cursos/vamos-a-programar-con-python.blade.php
@@ -46,10 +46,10 @@
                                 <div id="carouselExampleControls" class="carousel slide mb-4" data-ride="carousel">
                                     <div class="carousel-inner">
                                         <div class="carousel-item active">
-                                        <img class="d-block w-100" src="/assets/images/resource/python/nino-programando-2.jpg">
-                                        </div>                                        
-                                        <div class="carousel-item">
                                         <img class="d-block w-100" src="/assets/images/resource/python/pythoncode.jpg">
+                                        </div>
+                                        <div class="carousel-item">
+                                        <img class="d-block w-100" src="/assets/images/resource/python/nino-programando-2.jpg">
                                         </div>
                                         <div class="carousel-item">
                                         <img class="d-block w-100" src="/assets/images/resource/python/pythoncode.jpg">
